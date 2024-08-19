e8917aee1afe2a20c01b296a98d8c5f6b3f7232e
Ceyda Turkdogan <ceyda.turkdogan@gmail.com>
2024-08-20 00:01:38 +0300

    Add project files.

---
+++ ListGalaNight/Program.cs
@@ -0,0 +1,6 @@
+﻿List <string> galaNight=new List<string>() {"Bülent Ersoy","Ajda Pekkan","Ebru Gündeş","Hadise","Hande Yener","Tarkan","Funda Arar","Demet Akalın" };
+
+for (int i = 0; i < galaNight.Count; i++)
+{
+    Console.WriteLine($"{i+1}- {galaNight[i]}" );
+}
